456303837ea47fe5de13b6f96f2338e2b7f6d0a1
EnzoABarros <enzozobarros@gmail.com>
2025-04-04 15:01:12 -0300

    Imagens carrossel e alterações main.php

--- projeto/main.php
+++ projeto/main.php
@@ -12,10 +12,10 @@
     <title>Pentatonica</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-light navbar-light">
+<nav class="navbar navbar-expand-sm bg-light navbar-light fixed-top">
   <div class="container-fluid">
     <a class="navbar-brand" href="javascript:void(0)">
-        <img src="src/img/logo.png" alt="logo" style="width:120px;" href="main.php">
+        <img src="src/img/pentatonica.png" alt="logo" style="width:200px;" href="">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span class="navbar-toggler-icon"></span>
@@ -35,21 +35,21 @@
 
 <!-- Indicators/dots -->
 <div class="carousel-indicators">
-  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-  <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-  <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active border"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="1" class="border"></button>
+  <button type="button" data-bs-target="#demo" data-bs-slide-to="2" class="border"></button>
 </div>
 
 <!-- The slideshow/carousel -->
 <div class="carousel-inner">
   <div class="carousel-item active">
-    <img src="src/img/guitarras1.png.png" alt="Los Angeles" class="d-block w-100" style="width:50%;">
+    <img src="src/img/banner2.png" alt="Los Angeles" class="d-block w-100" style="width:50%;">
   </div>
   <div class="carousel-item">
-    <img src="src/img/guitarras1.png.png" alt="Chicago" class="d-block w-100" style="width:50%;">
+    <img src="src/img/banner1.png" alt="Chicago" class="d-block w-100" style="width:50%;">
   </div>
   <div class="carousel-item">
-    <img src="src/img/guitarras1.png.png" alt="New York" class="d-block w-100" style="width:50%;">
+    <img src="src/img/banner3.png" alt="New York" class="d-block w-100" style="width:50%;">
   </div>
 </div>
 
@@ -63,19 +63,32 @@
 </div>
 
 <div class="container">
-  <h1 class="h1 roboto-mono-fontelegal mt-5" style="font-size:50px;">Bem-vindo(a) à Pentatonica!</h1>
+  <h1 class="roboto-mono-fontelegal mt-5" style="font-size:50px;">Bem-vindo(a) à <kbd>pentatonica.<kbd></h1>
 </div>
 
-<div class="container-md mt-5 p-4 caixa">
-  <h3 class="h3">De guitarrista, para guitarrista.</h3>
-  <p>
-  Vai comprar sua primeira guitarra? Deseja aumentar sua coleção? Ou quer uma guitarra única? Independente da sua necessidade, temos o que você precisa para ficar
-  satisfeito com a sua compra
+<div class="d-flex justify-content-between py-5 px-5">
+  <div class="d-flex flex-column mt-5 py-4 caixa border">
+    <h3 class="h3 mb-5 ms-3">De guitarrista, para guitarrista.</h3>
+
+    <p class="mx-3">
+    Vai comprar sua primeira guitarra? Deseja aumentar sua coleção? Ou quer uma guitarra única? <br> Independente da sua necessidade, temos o que você precisa para ficar
+    satisfeito com a sua compra. <br> Vasculhe por nosso vasto catálogo de instrumentos para achar o que procura.
 
 
-  </p>
+    </p>
+  </div>
+
+  <div class="d-flex flex-column mt-5 py-4 caixa border">
+    <h3 class="h3 mb-5 ms-3">Precisa de ajuda para encontrar a guitarra <br> perfeita para você?</h3>
+
+    <p class="mx-3">
+    Vai comprar sua primeira guitarra? Deseja aumentar sua coleção? Ou quer uma guitarra única? <br> Independente da sua necessidade, temos o que você precisa para ficar
+    satisfeito com a sua compra. <br> Vasculhe por nosso vasto catálogo de instrumentos para achar o que procura.
+
+
+    </p>
+  </div>
 </div>
-
 
 </body>
 </html>
