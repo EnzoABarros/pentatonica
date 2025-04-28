0d2eb72e7f55700a2396db1a2dacb27b0c2f3e4d
EnzoABarros <enzozobarros@gmail.com>
2025-04-27 22:16:51 -0300

    Main arrumada

--- PROJETO/pentatonicaa/app/views/pages/main.php
+++ PROJETO/pentatonicaa/app/views/pages/main.php
@@ -24,8 +24,62 @@
     </div>
 
     <h1 class="titulo">
-        Bem-vindo( a ) à <span class="titulo-back">pentatonica.</span>
+        Bem-vindo(a) à <span class="titulo-back">pentatonica.</span>
     </h1>
+
+
+    <div style="background-color:rgb(241, 241, 241); padding-top: 0.9rem; padding-left: 0.5rem; padding-bottom: 0.3rem;">
+  <h3 style="font-size:35px;">De guitarrista, para guitarrista.</h3>
+
+  <p style="font-size:20px;">
+    Vai comprar sua primeira guitarra? Deseja aumentar sua coleção? Ou quer uma guitarra única? Independente da sua necessidade, temos o que você precisa para ficar
+    satisfeito com a sua compra. <br> Vasculhe por nosso vasto catálogo de instrumentos para achar o que procura.
+
+
+  </p>
+  </div>
+  <div style="background-color:rgb(224, 224, 224); padding-top: 0.9rem; padding-left: 0.5rem; padding-bottom: 0.3rem;">
+  <h3 style="font-size:35px;">Encontre a guitarra perfeita para você</h3>
+
+  <p style="font-size:20px;">
+    Não sabe qual guitarra escolher? Logo abaixo temos um guia sobre os modelos diferentes de guitarra e um quiz rápido para você encontrar o instrumento perfeito para você! <br>
+  </p>
+  </div>
+
+    <br><br><br><br><br><br><br><br><br><br>
+
+    <div class="guia">
+  <a class="guias" href="guia?guia=stratocaster">
+    <img src="/pentatonicaa/public/images/stratocaster.png" alt="">
+    </a>
+  <a class="guias" href="guia?guia=lespaul">
+    <img src="/pentatonicaa/public/images/lespaul.png" alt="">
+  </a>
+
+  <a class="guias" href="guia?guia=telecaster">
+    <img src="/pentatonicaa/public/images/telecaster.png" alt="">
+    </a>
+
+    <a class="guias" href="guia?guia=sg">
+    <img src="/pentatonicaa/public/images/sg.png" alt="">
+    </a>
+
+    <a class="guias" href="guia?guia=superstrato">
+    <img src="/pentatonicaa/public/images/superstratocaster.png" alt="">
+    </a>
+
+    <a class="guias" href="guia?guia=flyingv">
+    <img src="/pentatonicaa/public/images/flyingv.png" alt="">
+    </a>
+
+    <a class="guias" href="guia?guia=semiacustica">
+    <img src="/pentatonicaa/public/images/semiacustica.png" alt="">  
+    </a>
+
+    <a class="guias" href="guia?guia=quiz">
+      <img src="/pentatonicaa/public/images/quiz.png" alt="">
+    </a>
+</div>
 
 
     <script src="js/slider.js"></script>
